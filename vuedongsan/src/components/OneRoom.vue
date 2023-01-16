b1e327cda43b72c39df1a77c837cc00abc5b901a
박서연 <sypark@reflexion.co.kr>
2023-01-16 16:15:30 +0900

    axios 추가

--- vuedongsan/src/components/OneRoom.vue
+++ vuedongsan/src/components/OneRoom.vue
@@ -2,6 +2,7 @@
   <div id="one-room">
     <div v-for="(oneRoom, i) in oneRooms" :key="i">
       <h4>{{ oneRoom.product }}</h4>
+      <img :src="oneRoom.url" alt="" class="room-img">
       <p v-for="(price, j) in oneRoom.prices" :key="j">{{ price }} 만원</p>
       <button @click="report(oneRoom)">허위매물신고 {{ oneRoom.reportCnt }}</button>
     </div>
@@ -11,18 +12,21 @@
 </template>
 
 <script>
-const URL = "https://picsum.photos/v2/list?page=2&limit=100%E2%80%8B";
+const URL = "https://picsum.photos/v2/list?page=2&limit=100​";
 
 export default {
     name: 'OneRoom',
     data() {
         return {
             oneRooms: [
-                { product: '역삼동원룸', prices: [50, 60, 80], reportCnt: 0 },
-                { product: '천호동원룸', prices: [100], reportCnt: 0 },
-                { product: '마포구원룸', prices: [1000], reportCnt: 0 },
+                { product: '역삼동원룸', prices: [50, 60, 80], reportCnt: 0,url: '' },
+                { product: '천호동원룸', prices: [100], reportCnt: 0,url: '' },
+                { product: '마포구원룸', prices: [1000], reportCnt: 0 ,url: ''},
             ],
         }
+    },
+    mounted() {
+        this.fetchData();
     },
     methods: {
         priceHalf() {
@@ -37,21 +41,24 @@ export default {
         report(target) {
             target.reportCnt += 1;
         },
-        getData() {
+        fetchData() {
             this.$axios
-            .get(URL)
-            .then((res) => {
-                console.log(res.status)
-                console.log(res.data)
-            })
-            .catch((error)=> {
-                console.log(error)
-            })
-            .finally(() => {
-                console.log('마지막실행')
-            })
+                .get(URL)
+                .then((res) => {
+                    console.log(res.status)
+                    console.log(res.data)
+                    for (let i = 0; i < this.oneRooms.length; i++) {
+                        this.oneRooms[i].url = res.data[i].download_url;
+                    }
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+                .finally(() => {
+                    console.log('마지막실행')
+                })
         }
-    }
+    },
 }
 </script>
 
@@ -60,5 +67,10 @@ export default {
         display: flex;
         text-align: center;
         padding: 30px;
+    }
+    .room-img{
+        width: 200px;
+        height: 200px;
+        object-fit: cover;
     }
 </style>
